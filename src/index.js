418ed69a2efa12bc30a4eb28d8a1a397b47d5df4
Yury Markouski <everest2006@tut.by>
2017-09-14 23:46:51 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    var mas =[];
+    var count=0;
+    for (var i = 0;i<str.length;i++){
+        bracketsConfig.map(function (item,index) {
+            for (var y=0;y<item.length;y++){
+                if (str[i]===item[y]){
+                    if(y===0){
+                        if((mas[mas.length - 1] != item[y])||(item[0]!=item[1])) {
+                            mas.push(item[y]);
+                            break;
+                        }
+                    }
+                    if (y === 1) {
+                        if (mas[mas.length - 1] === item[y - 1]) {
+                            mas.pop();
+                        } else {
+                            mas.unshift('*');
+                        }
+                    }
+                }
+            }
+        })
+    }
+    return mas.length===0?true:false;
 }
